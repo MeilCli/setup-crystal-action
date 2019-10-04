d2ff18d370559656b74f6a371170f2ce86197803
meil <ics.sora.meil@gmail.com>
2019-10-05 01:12:26 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ export interface Option {
 function getOption(): Option {
     const crystalVersion = core.getInput("crystal_version", { required: true });
     let shardsVersion = core.getInput("shards_version", { required: true });
-    if (shardsVersion != "latest" && shardsVersion.startsWith("v") == false) {
+    if (
+        shardsVersion != "latest" &&
+        shardsVersion != "skip" &&
+        shardsVersion.startsWith("v") == false
+    ) {
         shardsVersion = `v${shardsVersion}`;
     }
     const githubToken = core.getInput("github_token", { required: true });
